6c22d33001d9dee3ab9953cc639433fae8ecbb72
Tong Anh Quan <anhquantong77@gmail.com>
2020-11-20 01:18:29 +0700

    90% too...
    Uncomment if you want to debug faster when only display raw text
    Send to alert to display data with notification effect
    Need to add option to rename user data to any name from user input

--- MMM-FaceNet/MMM-FaceNet.js
+++ MMM-FaceNet/MMM-FaceNet.js
@@ -7,16 +7,32 @@ Module.register("MMM-FaceNet", {
     },
     socketNotificationReceived: function(notification, payload) {
         if (notification == "FACENET_RESULT") {
-            // Split string "user - rate" from Python
-            var result = payload.split('-');
-            //console.log(result[0]); // getUser
-            console.log(result);
-            if (result.length != 0) {
-                document.getElementById("facenetresult").innerHTML = "Hello: ";
-                for (var i = 0; i < result.length; i++) {
-                    document.getElementById("facenetresult").innerHTML += result[i] + "-";
-                }
+            var splitSpecificUser = payload.split('/');
+            var userArr = [];
+            var rateArr = [];
+            var strCombine = "";
+
+            for(index in splitSpecificUser)
+            {
+                strCombine += splitSpecificUser[index] + "<br>";
             }
+
+            this.sendNotification("SHOW_ALERT",{type: "notification",title: "Hello" , message:"<h1>"+strCombine+"</h1>", timer : 2000});
+
+
+            // Only display text
+
+            // document.getElementById("facenetresult").innerHTML = "";
+            // for(index in splitSpecificUser)
+            // {
+            //     document.getElementById("facenetresult").innerHTML += splitSpecificUser[index] + "<br>";
+                
+                
+            //     //Uncomment if you want to do something
+            //     // var tmp = splitSpecificUser[index].split(',');
+            //     //userArr.push(tmp[0]);
+            //     //rateArr.push(tmp[1]); 
+            // }
         }
     },
     // Override dom generator.
@@ -24,7 +40,7 @@ Module.register("MMM-FaceNet", {
         var wrapper = document.createElement("div");
         var userRecog = document.createElement("span");
         userRecog.setAttribute("id", "facenetresult");
-        userRecog.innerHTML = "Chào Quân - Khánh";
+        userRecog.innerHTML = "...";
         userRecog.style.color = "pink";
         wrapper.appendChild(userRecog);
         return wrapper;
--- MMM-FaceNet/node_helper.js
+++ MMM-FaceNet/node_helper.js
@@ -9,37 +9,8 @@ module.exports = NodeHelper.create({
         console.log('[Xiu MMM-FACENET] Starting node_helper');
         // Create server to receive data via GET method
         this.expressApp.get('/facenetroute', function(req, res) {
-            var getUser = req.param('user');
-            var getNumOfUsers = req.param('quantity') - 1;
-            if (getUser != currentUser) {
-                currentUser = getUser;
-                if (index < getNumOfUsers) {
-                    // Save users into array
-                    userArr[index] = currentUser;
-                } else if (index == getNumOfUsers) {
-                    userArr[index] = currentUser;
-                    for (var i = 0; i < userArr.length; i++) {
-                        result += userArr[i] + "-";
-                    }
-                    // Send to MMM-FaceNet.js to display
-                    self.sendSocketNotification("FACENET_RESULT", result);
-                    // Reset
-                    result = "";
-                    currentUser = "";
-                }
-                index++;
-            }
-            // 2 different persons with the same result
-            else {
-                if (index < getNumOfUsers) {
-                    index++;
-                }
-            }
-            if (index > getNumOfUsers) {
-                index = 0;
-                result = "";
-                currentUser = "";
-            }
+            var getData = req.param('data');
+            self.sendSocketNotification("FACENET_RESULT", getData);
             res.send("OK"); // Don't delete, because it will help Python can send another GET requests
         });
     },
