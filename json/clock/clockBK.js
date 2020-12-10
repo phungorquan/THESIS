95d1da19467de526dd6269459f5ba7cde6de2552
Xiu <anhquantong77@gmail.com>
2020-12-10 22:04:01 +0700

    Add mmmvietnamcalendar and clock BK file

--- json/clock/clockBK.js
+++ json/clock/clockBK.js
@@ -3,24 +3,25 @@
     "header": "",
     "module": "clock",
     "config": {
-    	"displayType": "top_center",
-     	"displaySeconds": "true",
-    	"showPeriod": "false",
-    	"showPeriodUpper": "false",
-    	"clockBold": "false",
-    	"showDate": "true",
-    	"showWeek": "false",
+    	"displayType": "digital",
+     	"displaySeconds": false,
+    	"showPeriod": false,
+    	"showPeriodUpper": false,
+    	"clockBold": false,
+    	"showDate": true,
+    	"showWeek": false,
     	"dateFormat": "ddd, DD/MM",
-    	"lunarShow": "true",
+    	"lunarShow": true,
     	"analogSize": "200px",
     	"analogFace": "simple",
     	"analogPlacement": "bottom",
     	"analogShowDate": "top",
     	"secondsColor": "#111111",
-    	"timezone": "false",
-    	"showSunTimes": "false",
-    	"showMoonTimes": "false",
-    	"lat": "14.0583",
-    	"lon": "108.2772"
+    	"timezone": false,
+    	"showSunTimes": false,
+    	"showMoonTimes": false,
+    	"lat": 47.630539,
+    	"lon": -122.344147
     }
 }
+
--- json/mmmvietnamcalendar/mmmvietnamcalendarBK.js
+++ json/mmmvietnamcalendar/mmmvietnamcalendarBK.js
@@ -3,20 +3,29 @@
   "header": "",
   "module": "MMM-VietnamCalendar",
   "config": {
-    "maximumEntries": "7",
-    "maximumNumberOfDays": "365",
-    "showLocation": "true",
-    "maxTitleLength": "17",
-    "wrapEvents": "true",
-    "maxTitleLines": "3",
-    "fetchInterval": "60000",
-    "animationSpeed": "500",
-    "displayButton": "true",
+    "maximumEntries": 7,
+    "maximumNumberOfDays": 365,
+    "showLocation": true,
+    "maxTitleLength": 25,
+    "wrapEvents": false,
+    "maxTitleLines": 3,
+    "fetchInterval": 60000,
+    "animationSpeed": 500,
+    "displayButton": false,
     "dateEndFormat": "LT(DD/MM)",
     "defaultColor": "White",
     "lunarColor": "LightGreen",
-    "colored": "true",
+    "colored": true,
     "tableClass": "xsmall",
-    "lunarShow": "true"
+    "calendars": [{
+        "url": "https://calendar.google.com/calendar/ical/anhquantong77%40gmail.com/public/basic.ics",
+        "color": "Violet",
+        "name": "Lịch cá nhân của anhquantong77"
+    },
+    {
+      "url": "https://calendar.google.com/calendar/ical/quan.ng0anhin98%40gmail.com/public/basic.ics",
+      "color": "PowderBlue",
+      "name": "Lịch cá nhân của quan.ng0anhin98"
+    }]
   }
 }
