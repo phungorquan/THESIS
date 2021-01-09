96dcc9d2a77476bf8b17cba647497db895e15553
Xiu <anhquantong77@gmail.com>
2021-01-10 01:35:14 +0700

    Add icon,name on tab, add login funcs,html,image

---
+++ login.js
@@ -0,0 +1,37 @@
+var mySocketIO = io({transports: ['websocket'], upgrade: false});
+mySocketIO.on("WRONG_CREDENTIAL",function(error)
+{ 
+    alert("Sai tài khoản hoặc mật khẩu!");
+});
+
+mySocketIO.on("OK_CREDENTIAL",function(data)
+{ 
+    window.location.href = data;
+    //document.getElementById('idLogin').style.display = "none"; // Đóng form login
+});
+
+
+function Login()
+{
+    var getUsr = document.getElementById('idUsr').value;
+    var getPsw = document.getElementById('idPsw').value;
+
+    var arr = Array();
+    arr.push(getUsr);
+    arr.push(getPsw);
+    mySocketIO.emit("CHECKING_LOGIN",arr);
+}
+
+// Get the input field
+var input = document.getElementById("idLogin");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    Login();
+  }
+});
