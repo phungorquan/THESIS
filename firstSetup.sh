b040b996c29fe73757cfc525475a4669bdd2008b
Xiu <anhquantong77@gmail.com>
2021-01-25 01:23:17 +0700

    Add remove func to setup file

--- firstSetup.sh
+++ firstSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This file will create module .js file from BK.js files when first clone this server 
+# This file will create module.js files from BK.js files when first clone this server or remove them
 
 # Add all find result into an array
 fileArr=()
@@ -11,19 +11,42 @@ done < <(find . -name *BK.js -print0)
 # Get length of array
 len=${#fileArr[@]}
 
-# Copy one by one BK.js files to .js files
-for (( i=0; i<$len; i++ ))
-do 
-	backupFile=${fileArr[$i]}
-	blank=""
-	# Replace BK by a blank
-	FILE=${backupFile/BK/$blank}
-
-	# Check exist 
-	if [ -f "$FILE" ]; then
-	    echo "$FILE exists."
-	else 
-	    cp ${fileArr[$i]} ${backupFile/BK/$blank}
-	    echo "Create new $FILE"
+if [ "$1" != "" ]; then
+	if [ "$1" == "rmjs" ]; then
+		# Remove all .js files
+		for (( i=0; i<$len; i++ ))
+		do 
+			backupFile=${fileArr[$i]}
+			blank=""
+			# Replace BK by a blank
+			FILE=${backupFile/BK/$blank}
+		
+			# Check exist 
+			if [ -f "$FILE" ]; then
+			    rm $FILE
+			    echo "remove $FILE"
+			fi
+		done
+	elif [ "$1" == "cpjs" ]; then
+		# Copy one by one BK.js files to .js files
+		for (( i=0; i<$len; i++ ))
+		do 
+			backupFile=${fileArr[$i]}
+			blank=""
+			# Replace BK by a blank
+			FILE=${backupFile/BK/$blank}
+		
+			# Check exist 
+			if [ -f "$FILE" ]; then
+			    echo "$FILE exists."
+			else 
+			    cp ${fileArr[$i]} $FILE
+			    echo "Create new $FILE"
+			fi
+		done
 	fi
-done
+else 
+	echo "Put first parameter is"
+	echo "rmjs to remove all .js file"
+	echo "cpjs to copy backup.js files to .js files"
+fi
