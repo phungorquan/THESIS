52a3de9fd580a4f4ebe109753190c1109f9dba0e
Tong Anh Quan <anhquantong77@gmail.com>
2021-01-27 04:14:07 +0700

    Get all user > 0.5

--- FaceRecogFiles/inference/video_classifier.py
+++ FaceRecogFiles/inference/video_classifier.py
@@ -9,13 +9,23 @@ from .util import draw_bb_on_img
 from .constants import MODEL_PATH
 import requests
 import time
+import argparse
 
 # Command to run image process on Server: python -m inference.video_classifier
 
-def main():
 
-    #URL_STREAM = "/home/xiu/Facenet/face-recognition/DatasetAtUitLab1.mp4" # Local video from your computer
-    URL_STREAM = "http://192.168.1.18:8081" # Get image from Ras
+def parse_args():
+    parser = argparse.ArgumentParser(
+        'Input ip of Raspberry')
+    parser.add_argument('--ip', required=True, help='Ip of Raspberry')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    getIP = args.ip
+
+    #URL_STREAM = "/home/xiu/Facenet/face-recognition/3Nguoi.mp4" # Local video from your computer
+    URL_STREAM = "http://"+getIP+":8081/" # Get image from Ras
 
     cap = cv2.VideoCapture(URL_STREAM)
 
@@ -24,7 +34,7 @@ def main():
     strCombine = "" # String to combine all data which were recognized
     
     # Raspberry url to receive result from Server
-    URL = "http://192.168.1.18:8080/facenetroute" # Host from MMM-FaceNet
+    URL = "http://"+getIP+":8080/facenetroute" # Host from MMM-FaceNet
 
     while True:
         # Capture frame-by-frame
@@ -40,7 +50,7 @@ def main():
                     _user = face.top_prediction.label # Get name
                     _rate = float("{:.2f}".format(face.top_prediction.confidence)) # Get rate with 2 digits
 
-                    if _rate > 0.85:
+                    if _rate > 0.5:
                         flagExist = True
                         # Assign user to userArr if not exist
                         # Assign greater rate to user who was existed
@@ -61,32 +71,31 @@ def main():
                         if flagExist == False:
                             userArr.append(_user)
                             rateArr.append(_rate)
-                    else:
-                        userArr.append(" ") # Assign un-recognized people with blank space to have correctly behaviors later
-                        rateArr.append(_rate)
 
-                # Combine data        
-                currentData = ""
-                for index in range(len(userArr)):
-                    # With rate
-                    if index != (len(userArr) - 1):
-                        currentData = currentData + userArr[index] + "," + str(rateArr[index]) + "/"
-                    else:
-                        currentData = currentData + userArr[index] + "," + str(rateArr[index])
+                # If rate between 50 - threshold
+                if len(userArr) != 0:
+                    # Combine data        
+                    currentData = ""
+                    for index in range(len(userArr)):
+                        # With rate
+                        if index != (len(userArr) - 1):
+                            currentData = currentData + userArr[index] + "," + str(rateArr[index]) + "/"
+                        else:
+                            currentData = currentData + userArr[index] + "," + str(rateArr[index])
 
-                if currentData != strCombine:
-                    strCombine = currentData
-                    print("this is ", strCombine)
+                    if currentData != strCombine:
+                        strCombine = currentData
+                        print("this is ", strCombine)
 
-                    # Prepare data with feild 'data'
-                    PARAMS = {'data':strCombine}
+                        # Prepare data with feild 'data'
+                        PARAMS = {'data':strCombine}
 
-                    # Send to host 
-                    try:
-                        requests.get(url = URL, params = PARAMS) 
-                    except requests.ConnectionError as exception:
-                        print("URL is not exist, please check MMM")
-                    
+                        # Send to host 
+                        try:
+                            requests.get(url = URL, params = PARAMS) 
+                        except requests.ConnectionError as exception:
+                            print("URL is not exist, please check MMM")
+                        
                 # No need to draw if you are not debugging
                 draw_bb_on_img(faces, img)
                 # Display the resulting frame
@@ -98,7 +107,7 @@ def main():
                 requests.get(url = URL, params = {'data':"CAMERA_ERROR"}) 
             except requests.ConnectionError as exception:
                 print("CAMERA is ERROR, please check CAMERA")
-            time.sleep(5)
+            time.sleep(1)
             cap = cv2.VideoCapture(URL_STREAM)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
